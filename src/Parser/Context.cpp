58fb4422f77b50ac4a7071a05bb9fdc02453caba
hashem78 <hashem.olayano@gmail.com>
2022-10-06 02:22:11 +0300

    Remove unused methods and members from parsing context.

--- src/Parser/Context.cpp
+++ src/Parser/Context.cpp
@@ -2,27 +2,13 @@
 // Created by mythi on 01/09/22.
 //
 
-#include <stack>
-
 #include "Context.h"
 
 namespace hasha {
 
-    Context &Context::set_parsing_expression(bool val) noexcept {
-
-        parsing_expression = val;
-        return *this;
-    }
-
     Context &Context::set_parsing_args(bool val) noexcept {
 
         parsing_args = val;
-        return *this;
-    }
-
-    Context &Context::set_parsing_return_expression(bool val) noexcept {
-
-        parsing_return_expression = val;
         return *this;
     }
 
--- src/Parser/Context.h
+++ src/Parser/Context.h
@@ -10,16 +10,10 @@
 namespace hasha {
 
     struct Context {
-        bool parsing_expression;
         bool parsing_args;
-        bool parsing_return_expression;
         bool parsing_void_function;
 
-        Context &set_parsing_expression(bool val) noexcept;
-
         Context &set_parsing_args(bool val) noexcept;
-
-        Context &set_parsing_return_expression(bool val) noexcept;
 
         Context &set_parsing_void_function(bool val) noexcept;
 
