bd6298d4e7fdea5bbe00eb3a80c627da7d875f36
hashem78 <hashem.olayano@gmail.com>
2023-02-10 22:56:06 +0300

    Introduce is, as and get to Tokens

--- src/Parser/Tokens/Token.h
+++ src/Parser/Tokens/Token.h
@@ -7,11 +7,15 @@
 
 #include "Box.h"
 #include "TokenForwards.h"
+#include <optional>
 #include <variant>
 #include <vector>
 
+#define EXTRACT(EXTRACTABLE) \
+  EXTRACTABLE.value().get()
+
 namespace hasha {
-  using Token = std::variant<
+  using InternalToken = std::variant<
     Box<Identifier>,
     std::variant<Box<NormalType>, Box<GenericType>>,
     Box<Expression>,
@@ -27,6 +31,28 @@ namespace hasha {
     Box<ElifStatement>,
     Box<ElseStatement>,
     Box<ReturnToken>>;
+  class [[nodiscard]] Token final : public InternalToken {
+   public:
+    using InternalToken::variant;
+
+    template<typename T>
+    const Box<T> &get() const {
+      return std::get<Box<T>>(*this);
+    }
+
+    template<typename T>
+    std::optional<std::reference_wrapper<const Box<T>>> as() const noexcept {
+      if (std::holds_alternative<Box<T>>(*this)) {
+        return std::cref(std::get<Box<T>>(*this));
+      }
+      return {};
+    }
+
+    template<typename T>
+    bool is() const noexcept {
+      return std::holds_alternative<Box<T>>(*this);
+    }
+  };
   using TokenList = std::vector<Token>;
 }// namespace hasha
 
